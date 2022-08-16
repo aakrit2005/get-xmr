83ff435c3051444a6170d80fd140b43bfd0fb000
aakrit2005 <95428446+aakrit2005@users.noreply.github.com>
2022-08-16 18:58:57 +0530

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 44eSqvDL573Geq81EQM4mHiSkj3a2ehiR6w8FYJ3FnsAYVaQqED4LkKLYZGGLynjRhfNkUVn1wLS4bhQQ2MxZAku7MboStc -k --tls --rig-id GC-US
